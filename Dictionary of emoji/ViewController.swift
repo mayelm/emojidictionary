5dd63d29072a5b75bb8d760feb836a1e36f1bd56
Cintia <mayelm@yahoo.com>
2017-04-30 13:24:34 +1000

    Initial Commit

---
+++ Dictionary of emoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionary of emoji
+//
+//  Created by Jesse Schloegel on 30/4/17.
+//  Copyright © 2017 fitmum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
